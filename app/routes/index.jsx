e36a819fb5ecaf4c3380b62756f6da7caeaafbb8
ellena <sarveshghildiyal@gmail.com>
2024-01-06 18:47:27 +0530

    shadcn done

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,7 +1,9 @@
 export const meta = () => {
   return [{ title: "New Remix App" }];
 };
+
 import { Button } from "~/components/ui/button";
+
 export default function Index() {
   return (
     <div className="grid place-items-center h-screen">
--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,8 @@
 // Configuration for v1 routing
 import { createRoutesFromFolders } from '@remix-run/v1-route-convention';
 export default {
+  tailwind: true,
+  postcss: true,
   routes(defineRoutes) {
     return createRoutesFromFolders(defineRoutes);
   },
