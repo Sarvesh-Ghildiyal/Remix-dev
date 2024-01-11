126208f6a0a8cf36fa7102e1cbadd25b7a92435d
ellena <sarveshghildiyal@gmail.com>
2024-01-11 13:48:09 +0530

    route base set

---
+++ app/routes/admin/index.jsx
@@ -0,0 +1,9 @@
+import Layout from "~/Layouts/Layout";
+
+export default function admin() {
+  return (
+    <Layout>
+      <h2>hello buddy</h2>
+    </Layout>
+  );
+}
---
+++ app/routes/workers/index.jsx
@@ -0,0 +1,9 @@
+import Layout from '~/Layouts/Layout'
+
+export default function worker() {
+  return (
+    <Layout>
+      <div>worker</div>
+    </Layout>
+  );
+}
